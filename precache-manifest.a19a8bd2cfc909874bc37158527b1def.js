847ec308f4bb426b172939f4e6a8b78ed8325e65
Allen Kou <imakou@gmail.com>
2019-05-23 22:17:16 -0400

    front-end add config

---
+++ precache-manifest.a19a8bd2cfc909874bc37158527b1def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "920e48496dbb638abc54",
+    "url": "/static/js/main.d7ff5d7a.chunk.js"
+  },
+  {
+    "revision": "97d523df8ada2c9088f6",
+    "url": "/static/js/2.2337fd73.chunk.js"
+  },
+  {
+    "revision": "920e48496dbb638abc54",
+    "url": "/static/css/main.2dba6753.chunk.css"
+  },
+  {
+    "revision": "97d523df8ada2c9088f6",
+    "url": "/static/css/2.7d49b087.chunk.css"
+  },
+  {
+    "revision": "30e8c74392c262cf71c04bd89e1b9a7d",
+    "url": "/index.html"
+  }
+];
