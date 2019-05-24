03a9fa26ba3b0ee22ee87129a41d51c969fc9197
Allen Kou <imakou@gmail.com>
2019-05-23 22:58:27 -0400

    update: add homepage

---
+++ precache-manifest.0fc9aad3f679cc3634600019533bf614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71198013b722f4bb26e1",
+    "url": "/TodoList-GraphQL-Prod/static/js/runtime~main.82e43242.js"
+  },
+  {
+    "revision": "a73f41913f4c945aabbf",
+    "url": "/TodoList-GraphQL-Prod/static/js/main.c508a15d.chunk.js"
+  },
+  {
+    "revision": "e9abd69abb913c0dddb4",
+    "url": "/TodoList-GraphQL-Prod/static/js/2.343cd1c0.chunk.js"
+  },
+  {
+    "revision": "a73f41913f4c945aabbf",
+    "url": "/TodoList-GraphQL-Prod/static/css/main.2dba6753.chunk.css"
+  },
+  {
+    "revision": "e9abd69abb913c0dddb4",
+    "url": "/TodoList-GraphQL-Prod/static/css/2.7d49b087.chunk.css"
+  },
+  {
+    "revision": "d008b284f8559b2cc5ab72e9f6c91531",
+    "url": "/TodoList-GraphQL-Prod/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aff5df285f484c8f38197e97b51019a2.js"
+  "/TodoList-GraphQL-Prod/precache-manifest.0fc9aad3f679cc3634600019533bf614.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TodoList-GraphQL-Prod/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
